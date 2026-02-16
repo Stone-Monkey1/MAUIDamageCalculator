ffd9a2dc9c0468edd4baec25f4bb247412429e9e
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2026-02-16 10:01:31 -0500

    Add project files.

---
+++ MAUIDamageCalculator/SwordDamage.cs
@@ -0,0 +1,52 @@
+﻿
+namespace CalculateDamage
+{
+    internal class SwordDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        private int roll;
+        private bool flaming;
+        private bool magic;
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        public int Damage { get; private set; }
+
+
+
+        public bool Flaming { get { return flaming; } set { flaming = value; CalculateDamage(); } }
+
+        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
+
+
+
+        private void CalculateDamage()
+        {
+            decimal MagicMultiplier = 1M;
+            if (Magic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+            if (Flaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
